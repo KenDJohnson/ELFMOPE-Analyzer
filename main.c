3dd9185d0714047f22f39cc7291797df7f9de85a
Ken Johnson <johnso51@augsburg.edu>
2015-06-06 20:58:37 -0500

    detects elf and pe files

---
+++ main.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+
+#define PE  		1
+#define ELF  	 	2
+#define MACHO 	 	3
+
+
+int get_image_type(FILE *fp);
+void analyze_pe(FILE *fp);
+void analyze_elf(FILE *fp);
+
+int main(int argc, char **argv)
+{
+	FILE *fp;
+	char *fname;
+	int ftype;
+	void (*analyzer)(FILE *);
+
+	if ( argc < 2 )
+	{
+		printf( "Usage: %s: <executable>\n", argv[0] );
+		return 1;
+	}
+	fname = argv[1];
+	fp = fopen( fname, "rb" );
+	
+	ftype = get_image_type(fp);
+	if(  	 ftype == PE    ) analyzer = &analyze_pe; 
+	else if( ftype == ELF   ) analyzer = &analyze_elf; 
+	else if( ftype == MACHO ) printf( "Mach-O file detected.\n" ); 
+	else {
+		printf("Unknown file type.\n");
+		return 1;
+	}
+	analyzer(fp);
+
+
+	fclose( fp );
+	return 0;
+}
+
+
+void analyze_pe(FILE *fp)
+{
+	printf("In PE\n");
+}
+
+void analyze_elf(FILE *fp)
+{
+	printf("In ELF\n");
+}
+
+
+int get_image_type(FILE *fp)
+{
+	unsigned char sig[4];
+	size_t 	size;
+	
+	size = fread( &sig, 1, 4, fp );
+	if( size != 4 )
+	{
+		fprintf(stderr, "[-] Error reading file header.\n");
+		return -1;
+	}
+	
+	/* Debug output for the header */
+	fprintf(stderr, "0x");
+	for( int i = 0; i < 4; i++ )
+	{
+		fprintf(stderr, "%2x", sig[i]);
+	}
+	fprintf(stderr, "\n");
+
+	 if( sig[0] == 0x4D &&
+		 sig[1] == 0x5A &&
+		 sig[2] == 0x90 &&
+		 sig[3] == 0x00   )
+	{
+		return PE;
+	}
+	else if( sig[0] == 0x7F &&
+			 sig[1] == 0x45 &&
+			 sig[2] == 0x4C &&
+			 sig[3] == 0x46   )
+	{
+		return ELF;
+	}
+
+	return 0;
+}
